56878eb4560e8205b2519e3166262cac7eeb0632
Kha N. Do <donguyenkha@gmail.com>
2017-12-15 08:45:07 +0900

    Update project dep

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const restify = require('restify');
-
+const util = require('./models/util');
 const app = restify.createServer();
 
+// Hello message
 app.get('/', function (req, res) {
   res.send(200, {
     message: 'kcoin blockchain api by Kha Do'
   });
 });
 
+// Add new transaction => Broadcast new transaction
+
+// Add new block => Broadcast new block
+
+// Get all block in main chain. TODO: Pagination
+
+// Get a block by hash or seq in main chain
+
+// Get list of waiting transaction
+
+// Get a transaction by ?
+
 const port = process.env.PORT || 5000;
 
+// Starting HTTP server
 app.listen(port)
 
 console.log('Server is listening on port', port);
